c3048d9e81b4305b7d2ae87cc31ca2f0288f52a1
Sam Marx <smarx@moxiesoft.com>
2013-03-25 16:00:42 -0500

    added extra (external file) create, destroy, get and list

--- lib/stingray/extra.rb
+++ lib/stingray/extra.rb
@@ -1,17 +1,35 @@
 module Stingray
-  class LoadBalancer
-    def initalize
-      @lb||=Stingray::Config.new
-      puts @lb
-    end
-    def get_servers
-
-    end
+  class Extra
+    include Stingray::ServiceInterface
+    attr_accessor :name, :files, :content
     
-    def create_lb
+    # Get a list of all files
+    def list_files
+      @files=get_endpoint('extra').keys
     end
 
-    def delete_lb
+    # get one specific file
+    def file(name)
+      @name=name
+      @content=get_rest["extra/#{@name}"].get rescue nil
+    end
+
+    # create a new file
+    def create(name,content='')
+      @name=name
+      @content=content
+    end
+
+    # Save the current file.  
+    def save
+      return if @content.nil?
+      get_rest["extra/#{@name}"].put @content, :content_type => "application/octet-stream"
+    end
+
+    # destroy the current file.
+    def destroy
+      return if @name.nil?
+      get_rest["extra/#{@name}"].delete 
     end
 
 
